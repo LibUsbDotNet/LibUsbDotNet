3d7a0cd595387aa9982568e3cddf9230808044d8
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-09-21 11:32:57 +0200

    Add an UsbDevice class which wraps around a libusb device handle

---
+++ src/LibUsbDotNet/LibUsb/UsbDevice.Device.cs
@@ -0,0 +1,148 @@
+﻿using LibUsbDotNet.Info;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace LibUsbDotNet.LibUsb
+{
+    public partial class UsbDevice
+    {
+        private readonly Device device;
+
+        private UsbDeviceInfo descriptor;
+
+        public unsafe UsbDeviceInfo Descriptor
+        {
+            get
+            {
+                this.EnsureNotDisposed();
+
+                if (this.descriptor == null)
+                {
+                    DeviceDescriptor descriptor;
+                    NativeMethods.GetDeviceDescriptor(this.device, &descriptor).ThrowOnError();
+                    this.descriptor = UsbDeviceInfo.FromUsbDeviceDescriptor(this, descriptor);
+                }
+
+                return this.descriptor;
+            }
+        }
+
+        public ReadOnlyCollection<UsbConfigInfo> Configs
+        {
+            get
+            {
+                return this.Descriptor.Configurations;
+            }
+        }
+
+        public unsafe UsbConfigInfo ActiveConfigDescriptor
+        {
+            get
+            {
+                this.EnsureNotDisposed();
+
+                ConfigDescriptor* list = null;
+                UsbConfigInfo value = null;
+
+                try
+                {
+                    NativeMethods.GetActiveConfigDescriptor(this.device, &list).ThrowOnError();
+                    value = UsbConfigInfo.FromUsbConfigDescriptor(this, list[0]);
+                    return value;
+                }
+                finally
+                {
+                    if (list != null)
+                    {
+                        NativeMethods.FreeConfigDescriptor(list);
+                    }
+                }
+            }
+        }
+
+        public byte BusNumber
+        {
+            get
+            {
+                return NativeMethods.GetBusNumber(this.device);
+            }
+        }
+
+        public byte PortNumber
+        {
+            get
+            {
+                return NativeMethods.GetPortNumber(this.device);
+            }
+        }
+
+        public unsafe List<byte> PortNumbers
+        {
+            get
+            {
+                byte[] portNumbers = new byte[8];
+
+                fixed (byte* ptr = portNumbers)
+                {
+                    NativeMethods.GetPortNumbers(this.device, ptr, portNumbers.Length).ThrowOnError();
+                }
+
+                return new List<byte>(portNumbers);
+            }
+        }
+
+        public UsbDevice GetParent()
+        {
+            var parent = NativeMethods.GetParent(this.device);
+            return new UsbDevice(parent);
+        }
+
+        public byte Address
+        {
+            get
+            {
+                return NativeMethods.GetDeviceAddress(this.device);
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return NativeMethods.GetDeviceSpeed(this.device);
+            }
+        }
+
+        public int GetMaxPacketSize(byte endPoint)
+        {
+            return NativeMethods.GetMaxPacketSize(this.device, endPoint);
+        }
+
+        public int GetMaxIsoPacketSize(byte endPoint)
+        {
+            return NativeMethods.GetMaxIsoPacketSize(this.device, endPoint);
+        }
+
+        public unsafe UsbConfigInfo GetConfigDescriptor(byte configIndex)
+        {
+            this.EnsureNotDisposed();
+
+            ConfigDescriptor* list = null;
+            UsbConfigInfo value = null;
+
+            try
+            {
+                NativeMethods.GetConfigDescriptor(this.device, configIndex, &list).ThrowOnError();
+                value = UsbConfigInfo.FromUsbConfigDescriptor(this, list[0]);
+                return value;
+            }
+            finally
+            {
+                if (list != null)
+                {
+                    NativeMethods.FreeConfigDescriptor(list);
+                }
+            }
+        }
+    }
+}
---
+++ src/LibUsbDotNet/LibUsb/UsbDevice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LibUsbDotNet.LibUsb
+{
+    public partial class UsbDevice : IDisposable, ICloneable
+    {
+        private bool disposed;
+
+        public UsbDevice(Device device)
+        {
+            this.device = device ?? throw new ArgumentNullException(nameof(device));
+        }
+
+        public UsbDevice Clone()
+        {
+            return new UsbDevice(NativeMethods.RefDevice(this.device));
+        }
+
+        object ICloneable.Clone()
+        {
+            return this.Clone();
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            // Close the libusb_device_handle if required.
+            this.Close();
+
+            // Close the libusb_device handle.
+            this.device.Dispose();
+
+            this.disposed = true;
+        }
+
+        public override string ToString()
+        {
+            if (this.IsOpen)
+            {
+                return this.Descriptor.ToString();
+            }
+            else
+            {
+                return base.ToString();
+            }
+        }
+
+        protected void EnsureNotDisposed()
+        {
+            if (this.disposed)
+            {
+                throw new ObjectDisposedException(nameof(UsbDevice));
+            }
+        }
+    }
+}
