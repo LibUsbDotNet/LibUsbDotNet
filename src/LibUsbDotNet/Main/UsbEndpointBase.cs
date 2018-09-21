768e18557d8d1772c2146e5625d778f2137a4f31
Sylvain Fasel <sylvain.fasel@ellisys.com>
2018-09-21 14:52:35 +0200

    Dispose UsbTransferContext of UsbEnpointBase (UsbEnpointBase.mTransferContext) when UsbEnpointBase is disposed

--- src/LibUsbDotNet/Main/UsbEndpointBase.cs
+++ src/LibUsbDotNet/Main/UsbEndpointBase.cs
@@ -381,6 +381,15 @@ namespace LibUsbDotNet.Main
                     if (epReader.DataReceivedEnabled) epReader.DataReceivedEnabled = false;
                 }
                 Abort();
+
+	            if (!ReferenceEquals(mTransferContext, null))
+	            {
+		            mTransferContext.Dispose();
+		            mTransferContext = null;
+	            }
+
+				mUsbDevice.ActiveEndpoints.RemoveFromList(this);
+
                 mUsbDevice.ActiveEndpoints.RemoveFromList(this);
             }
             mIsDisposed = true;
