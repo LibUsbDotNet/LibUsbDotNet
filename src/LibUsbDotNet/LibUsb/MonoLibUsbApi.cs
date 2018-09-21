d566c7c0b6234d94c0fc37d52cfa7a5763f45893
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-09-21 18:47:21 +0200

    Remove unused methods

--- src/LibUsbDotNet/LibUsb/MonoLibUsbApi.cs
+++ src/LibUsbDotNet/LibUsb/MonoLibUsbApi.cs
@@ -20,11 +20,9 @@
 // visit www.gnu.org.
 // 
 // 
+using LibUsbDotNet;
 using System;
 using System.Runtime.InteropServices;
-using LibUsbDotNet;
-using MonoLibUsb.Descriptors;
-using MonoLibUsb.Profile;
 
 namespace MonoLibUsb
 {
@@ -80,137 +78,12 @@ namespace MonoLibUsb
 
 #region API LIBRARY FUNCTIONS - USB descriptors
 
-        /// <summary>
-        /// Gets the standard device descriptor.
-        /// </summary>
-        /// <remarks>
-        /// <note type="tip">This is a non-blocking function which does not involve any requests being sent to the device.</note>
-        /// <note type="tip" title="Libusb-1.0 API:"><seelibusb10 group="desc"/></note>
-        /// </remarks>
-        /// <param name="deviceProfileHandle">A device profile handle.</param>
-        /// <param name="deviceDescriptor">The <see cref="MonoUsbDeviceDescriptor"/> clas that will hold the data.</param>
-        /// <returns>0 on success or a <see cref="MonoUsbError"/> code on failure.</returns>
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_get_device_descriptor")]
-        public static extern int GetDeviceDescriptor([In] MonoUsbProfileHandle deviceProfileHandle, [Out] DeviceDescriptor deviceDescriptor);
-
-        /// <summary>
-        /// Get the USB configuration descriptor for the currently active configuration.
-        /// </summary>
-        /// <remarks>
-        /// <note type="tip">This is a non-blocking function which does not involve any requests being sent to the device.</note>
-        /// <note type="tip" title="Libusb-1.0 API:"><seelibusb10 group="desc"/></note>
-        /// </remarks>
-        /// <param name="deviceProfileHandle">A device profile handle.</param>
-        /// <param name="configHandle">A config handle.</param>
-        /// <returns>
-        /// <list type="bullet">
-        /// <item>0 on success</item>
-        /// <item><see cref="MonoUsbError.ErrorNotFound"/> if the device is in unconfigured state </item>
-        /// <item>another <see cref="MonoUsbError"/> code on error</item>
-        /// </list>
-        /// </returns>
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_get_active_config_descriptor")]
-        public static extern int GetActiveConfigDescriptor([In] MonoUsbProfileHandle deviceProfileHandle, [Out] out MonoUsbConfigHandle configHandle);
-
-        /// <summary>
-        /// Get a USB configuration descriptor based on its index. 
-        /// </summary>
-        /// <remarks>
-        /// <note type="tip">This is a non-blocking function which does not involve any requests being sent to the device.</note>
-        /// <note type="tip" title="Libusb-1.0 API:"><seelibusb10 group="desc"/></note>
-        /// </remarks>
-        /// <param name="deviceProfileHandle">A device profile handle.</param>
-        /// <param name="configIndex">The index of the configuration you wish to retrieve.</param>
-        /// <param name="configHandle">A config handle.</param>
-        /// <returns>
-        /// <list type="bullet">
-        /// <item>0 on success</item>
-        /// <item><see cref="MonoUsbError.ErrorNotFound"/> if the device is in unconfigured state </item>
-        /// <item>another <see cref="MonoUsbError"/> code on error</item>
-        /// </list>
-        /// </returns>
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_get_config_descriptor")]
-        public static extern int GetConfigDescriptor([In] MonoUsbProfileHandle deviceProfileHandle, byte configIndex, [Out] out MonoUsbConfigHandle configHandle);
-
-        /// <summary>
-        /// Get a USB configuration descriptor with a specific bConfigurationValue.
-        /// </summary>
-        /// <remarks>
-        /// <note type="tip">This is a non-blocking function which does not involve any requests being sent to the device.</note>
-        /// <note type="tip" title="Libusb-1.0 API:"><seelibusb10 group="desc"/></note>
-        /// </remarks>
-        /// <param name="deviceProfileHandle">A device profile handle.</param>
-        /// <param name="bConfigurationValue">The bConfigurationValue of the configuration you wish to retrieve.</param>
-        /// <param name="configHandle">A config handle.</param>
-        /// <returns>
-        /// <list type="bullet">
-        /// <item>0 on success</item>
-        /// <item><see cref="MonoUsbError.ErrorNotFound"/> if the device is in unconfigured state </item>
-        /// <item>another <see cref="MonoUsbError"/> code on error</item>
-        /// </list>
-        /// </returns>
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_get_config_descriptor_by_value")]
-        public static extern int GetConfigDescriptorByValue([In] MonoUsbProfileHandle deviceProfileHandle, byte bConfigurationValue, [Out] out MonoUsbConfigHandle configHandle);
-
         [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_free_config_descriptor")]
         internal static extern void FreeConfigDescriptor(IntPtr pConfigDescriptor);
 
 #endregion
 
 #region API LIBRARY FUNCTIONS - Device handling and enumeration (part 2)
-
-        /// <summary>
-        /// Get the number of the bus that a device is connected to. 
-        /// </summary>
-        /// <remarks>
-        /// <note type="tip" title="Libusb-1.0 API:"><seelibusb10 group="dev"/></note>
-        /// </remarks>
-        /// <returns>The bus number.</returns>
-        /// <param name="deviceProfileHandle">A device profile handle.</param>
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_get_bus_number")]
-        public static extern byte GetBusNumber([In] MonoUsbProfileHandle deviceProfileHandle);
-
-        /// <summary>
-        /// Get the address of the device on the bus it is connected to. 
-        /// </summary>
-        /// <remarks>
-        /// <note type="tip" title="Libusb-1.0 API:"><seelibusb10 group="dev"/></note>
-        /// </remarks>
-        /// <returns>The device address.</returns>
-        /// <param name="deviceProfileHandle">A device profile handle.</param>
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_get_device_address")]
-        public static extern byte GetDeviceAddress([In] MonoUsbProfileHandle deviceProfileHandle);
-
-        /// <summary>
-        /// Convenience function to retrieve the wMaxPacketSize value for a particular endpoint in the active device configuration. 
-        /// </summary>
-        /// <param name="deviceProfileHandle">A device profile handle.</param>
-        /// <param name="endpoint">Endpoint address to retrieve the max packet size for.</param>
-        /// <remarks>
-        /// <note type="tip" title="Libusb-1.0 API:"><seelibusb10 group="dev"/></note>
-        /// <para>This function was originally intended to be of assistance when setting up isochronous transfers, but a design mistake resulted in this function instead. It simply returns the <see cref="MonoUsbEndpointDescriptor.wMaxPacketSize"/> value without considering its contents. If you're dealing with isochronous transfers, you probably want <see cref="GetMaxIsoPacketSize"/> instead.</para>
-        /// </remarks>
-        /// <returns>The <see cref="MonoUsbEndpointDescriptor.wMaxPacketSize"/></returns>
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_get_max_packet_size")]
-        public static extern int GetMaxPacketSize([In] MonoUsbProfileHandle deviceProfileHandle, byte endpoint);
-
-        /// <summary>
-        /// Calculate the maximum packet size which a specific endpoint is capable is sending or receiving in the duration of 1 microframe.
-        /// </summary>
-        /// <remarks>
-        /// <para>Only the active configuration is examined. The calculation is based on the wMaxPacketSize field in the endpoint descriptor as described in section 9.6.6 in the USB 2.0 specifications.</para>
-        /// <para>If acting on an isochronous or interrupt endpoint, this function will multiply the value found in bits 0:10 by the number of transactions per microframe (determined by bits 11:12). Otherwise, this function just returns the numeric value found in bits 0:10.</para>
-        /// <para>This function is useful for setting up isochronous transfers, for example you might pass the return value from this function to <see  cref="MonoUsbTransfer.SetIsoPacketLengths">libusb_set_iso_packet_lengths</see> in order to set the length field of every isochronous packet in a transfer.</para>
-        /// <note type="tip" title="Libusb-1.0 API:"><seelibusb10 group="dev"/></note>
-        /// </remarks>
-        /// <param name="deviceProfileHandle">A device profile handle.</param>
-        /// <param name="endpoint">Endpoint address to retrieve the max packet size for.</param>
-        /// <returns>The maximum packet size which can be sent/received on this endpoint.</returns>
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_get_max_iso_packet_size")]
-        public static extern int GetMaxIsoPacketSize([In] MonoUsbProfileHandle deviceProfileHandle, byte endpoint);
-
-        [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_open")]
-        internal static extern int Open([In] MonoUsbProfileHandle deviceProfileHandle, ref IntPtr deviceHandle);
 
         [DllImport(NativeMethods.LibUsbNativeLibrary, CallingConvention = NativeMethods.LibUsbCallingConvention, SetLastError = false, EntryPoint = "libusb_close")]
         internal static extern void Close(IntPtr deviceHandle);
