c2f65ab127ce5b19112f6f9cb824c8d4fc940332
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-09-25 19:13:31 +0200

    Remove unused files

--- src/build/mergeChangeHistory.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-ECHO Merging 'LibUsbDotNet.Change.History.Current.aml' into 'LibUsbDotNet.Change.History.aml'..
-CALL make.bat MergeChangeHistory
--- src/build/updateRevisionFromSvn.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-REM Gets the revision number from the libusbdotnet SVN repository.
-REM Updates the revision field in the 'LibUsbDotNet.cfg' config file.
-CALL make.bat UpdateRevisionFromSvn
