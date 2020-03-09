1a9497ac5be3c16de7acead0c977aa38f0862077
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-03-09 16:10:51 +0100

    Delete Travis CI integration

--- ci/build-linux.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-schroot -c $RID -- VERSION=$VERSION; RID=$RID; echo Running on version $VERSION with rid $RID; ci/build.sh
--- ci/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd src/
-dotnet pack LibUsbDotNet/LibUsbDotNet.csproj -c Release
-dotnet test LibUsbDotNet.Tests/LibUsbDotNet.Tests.csproj /p:RuntimeIdentifier=$RID
