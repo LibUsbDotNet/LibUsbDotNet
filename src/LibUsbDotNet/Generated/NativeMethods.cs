db76c3d94119467ad816b39fbb54d56772442770
exelix <ex.elix@libero.it>
2020-07-27 15:52:08 +0200

    build for linux-arm

--- src/LibUsbDotNet/Generated/NativeMethods.cs
+++ src/LibUsbDotNet/Generated/NativeMethods.cs
@@ -45,7 +45,7 @@ namespace LibUsbDotNet
 #if WIN_X64 || WIN_X86 || NET45 || WIN7_X64 || WIN7_X86 // win7-x64, win7-x64 during testing only.
         internal const string LibUsbNativeLibrary = "libusb-1.0.dll";
         internal const CallingConvention LibUsbCallingConvention = CallingConvention.StdCall;
-#elif LINUX_X64 || LINUX_ARM64 || UBUNTU || UBUNTU_16_04_X64 || UBUNTU_16_04_ARM64 // ubuntu during testing only.
+#elif LINUX_X64 || LINUX_ARM64 || LINUX_ARM || UBUNTU || UBUNTU_16_04_X64 || UBUNTU_16_04_ARM64 // ubuntu during testing only.
         internal const string LibUsbNativeLibrary ="libusb-1.0.so.0";
         internal const CallingConvention LibUsbCallingConvention = CallingConvention.Cdecl;
 #elif OSX_X64 || OSX_10_12_X64 // osx during testing only
